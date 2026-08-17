8244a840a193a4beef683101e4cdb8f9848faea0
GilbertoMej <gilberto.mejia@colimasoft.com>
2026-08-17 09:05:02 -0600

    [Feature] Setup my agent for testing after finishing the labs

---
+++ my-agent/agent.py
@@ -0,0 +1,20 @@
+# import python SDK
+from openai import OpenAI
+
+# Create client
+client = OpenAI(
+    base_url="YOUR_BASE_URL",
+    api_key="YOUR_API_KEY"
+)
+
+# First API Call
+response = client.chat.completions.create(
+    model="openai/gpt-4.1",
+    messages=[
+        {"role": "system",
+         "content": "Be concise and friendly."},
+        {"role": "user", "content": "What is an AI agent?"}
+        ]
+)
+
+print(response.choices[0].message.content)
