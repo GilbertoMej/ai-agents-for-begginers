b84cdc05dcd22ab8107ad598cdf97182a907e104
GilbertoMej <gilberto.mejia@colimasoft.com>
2026-08-17 09:14:44 -0600

    [Feature] Add loop to starter file in the agent components

--- wire-up-components/agent_components.py
+++ wire-up-components/agent_components.py
@@ -11,4 +11,12 @@ model = "openai/gpt-4.1-mini"
 messages = []
 
 # Add the while loop here
-
+while True:
+    response = client.chat.completions.create(
+        model=model,
+        messages=messages,
+    )
+    finish_reason = response.choices[0].finish_reason
+    if finish_reason == "stop":
+        print(response.choices[0].message.content)
+        break
